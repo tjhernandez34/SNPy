7af9c9518ed0622700831d5e04efd32ad95a7cd7
Osama Zayyad <o.zayyad@gmail.com>
2014-07-26 16:32:38 -0500

    fixed heroku stuff

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,8 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
 if ENV["REDISCLOUD_URL"]
 	$redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
+# $redis = Redis.new(:host => 'localhost', :port => 6379)
+
 
 
 # redis://rediscloud:LjDZwhZSysSY3vWK@pub-redis-17077.us-east-1-1.2.ec2.garantiadata.com:17077
