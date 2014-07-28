eaf83504fafd79cd2e61798b8b4abf356de66f6a
Osama Zayyad & Travis Hernandez <pair+ozayyad.tjhernandez34@devbootcamp.com>
2014-07-28 11:39:00 -0500

    parse algorithm refactoring

--- app/controllers/genomes_controller.rb
+++ app/controllers/genomes_controller.rb
@@ -9,6 +9,8 @@ class GenomesController < ApplicationController
 
 	def create
 		@genome = Genome.new(genome_params)
+    text_file = params[:genome][:file_url].read
+    parse(text_file)
     respond_to do |format|
       if @genome.save
         format.html {redirect_to '/user/profile', notice: "Genome was successfully uploaded." }
@@ -17,6 +19,26 @@ class GenomesController < ApplicationController
       end
     end
 	end
+
+  def parse(file)
+      report = Report.create(genome_id: @genome.id)
+      file.each_line do |line|
+        snp = line.scan(/(^rs\d+|^i\d+)/)
+        allele = line.scan(/\s([A,T,G,C]{2})(\s|\z)/)
+        if snp != "" && allele != ""
+          snp = snp.join.strip
+          allele = allele.join.strip
+          $redis.hset(current_user.username, snp, allele)
+        end
+      end
+
+      Marker.all.each do |marker|
+        if $redis.hget(current_user.username, marker.snp) == marker.allele
+          Risk.create(report_id: report.id, marker_id: marker.id)
+        end
+      end
+      $redis.del(current_user.username)
+    end
 
 	private
 	def genome_params
