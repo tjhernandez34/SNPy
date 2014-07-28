bfb90574678ee995d2f4b14045d69e7f1a4954ec
Osama Zayyad & Travis Hernandez <pair+ozayyad.tjhernandez34@devbootcamp.com>
2014-07-28 13:25:19 -0500

    fixed redis cloud thing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 if ENV["REDISCLOUD_URL"]
 	$redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+# $redis = Redis.new(:host => 'localhost', :port => 6379)
 
 
 
