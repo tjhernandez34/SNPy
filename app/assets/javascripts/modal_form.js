744e271fdc00e0f234f2853ff7a82ddb8688fbf1
Natalie McCroy <nataliemccroy@gmail.com>
2014-07-28 17:05:14 -0500

    add styling to upload page

--- app/assets/javascripts/modal_form.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function(){
-//     $('.new_user').submit(function(e){
-//       return false
-//     });
-//   $('#modaltrigger').leanModal({top: 110, overlay: 0.45});
-// });
-
