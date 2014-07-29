9b6e4c345ff6254eb3b1906892026e5fb412e912
Osama Zayyad & Travis Hernandez <pair+ozayyad.tjhernandez34@devbootcamp.com>
2014-07-29 09:02:46 -0500

    rocking

--- app/controllers/genomes_controller.rb
+++ app/controllers/genomes_controller.rb
@@ -53,10 +53,11 @@ class GenomesController < ApplicationController
 
       puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
       data = open("https://s3.amazonaws.com/#{params[:bucket]}/#{params[:key]}") 
-      send_data data.read, :filename => "#{params[:key]}", :disposition => 'attachment', :stream => 'true', :buffer_size => '4096' 
+      puts "#{data.read}"
+      puts send_data data.read, :filename => "#{params[:key]}", :disposition => 'attachment', :stream => 'true', :buffer_size => '4096' 
 
-      @file = URI.parse(params[:key])
-      puts @file.open
+
+      # puts @file.open
       puts "---------------------------"
       puts "#{@file.read}"
       puts "#{@file.body.read}"
