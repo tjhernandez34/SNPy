03f990b4ba075bff1adbdfbd38edc401effcf613
Osama Zayyad & Travis Hernandez <pair+ozayyad.tjhernandez34@devbootcamp.com>
2014-07-29 15:50:22 -0500

    testing whether it parses a large file

--- app/controllers/genomes_controller.rb
+++ app/controllers/genomes_controller.rb
@@ -16,37 +16,6 @@ class GenomesController < ApplicationController
     redirect_to '/user/profile'
   end
 
-  # def parse(report)
-  #     puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  #     puts params[:bucket]
-  #     puts params[:key] 
-
-  #     puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  #     data = open("https://s3.amazonaws.com/#{params[:bucket]}/#{params[:key]}") 
-  #     puts "--------------------------------------------------"
-  #     data.read.each_line do |line|
-  #       puts "x"
-  #       snp = line.scan(/(^rs\d+|^i\d+)/)
-  #       allele = line.scan(/\s([A,T,G,C]{2})(\s|\z)/)
-  #       if snp != "" && allele != ""
-  #         puts "y"
-  #         snp = snp.join.strip
-  #         allele = allele.join.strip
-  #         $redis.hset(current_user.username, snp, allele)
-  #       end
-
-  #     end
-
-  #     Marker.all.each do |marker|
-  #       if $redis.hget(current_user.username, marker.snp) == marker.allele
-  #         puts "z"
-  #         Risk.create(report_id: report.id, marker_id: marker.id)
-  #       end
-  #     end
-  #     $redis.del(current_user.username)
-      
-  #   end
-
 	private
 	def genome_params
     params.require(:genome).permit(:user_id, :file_url, :first_name, :last_name, :username)
--- app/models/report.rb
+++ app/models/report.rb
@@ -7,33 +7,22 @@ class Report < ActiveRecord::Base
   attr_reader :create_report
 
   def parse(bucket, key, user)
-    puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-
     data = open("https://s3.amazonaws.com/#{bucket}/#{key}") 
-    puts "--------------------------------------------------"
     data.read.each_line do |line|
-      puts "x"
       snp = line.scan(/(^rs\d+|^i\d+)/)
       allele = line.scan(/\s([A,T,G,C]{2})(\s|\z)/)
       if snp != "" && allele != ""
-        puts "y"
         snp = snp.join.strip
         allele = allele.join.strip
         $redis.hset(user.username, snp, allele)
       end
-   puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     end
-
     Marker.all.each do |marker|
       if $redis.hget(user.username, marker.snp) == marker.allele
-        puts "z"
         Risk.create(report_id: self.id, marker_id: marker.id)
       end
     end
+    puts 'z'
     $redis.del(user.username)
-    
   end
-
-
-
 end
