3e8797467f6087e55654fc36f3b21989e77aae2a
Osama Zayyad & Travis Hernandez <pair+ozayyad.tjhernandez34@devbootcamp.com>
2014-07-28 23:08:52 -0500

    to

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
-if ENV["REDISCLOUD_URL"]
-	$redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
-end
+# if ENV["REDISCLOUD_URL"]
+# 	$redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+# end
 $redis = Redis.new(:host => 'localhost', :port => 6379)
 
 
