94f306cfcce216bb0adb94a58899f9346522694d
Natalie McCroy & Travis Hernandez <pair+nataliemac81.tjhernandez34@devbootcamp.com>
2014-07-30 17:30:57 -0500

    homepage buttons

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,9 +1,29 @@
-$(document).ready(function() {
-$('.login').on('click', function(event){
-  event.preventDefault();
-  console.log("displaay")
-$("#home_login_form").css('visibility','visible');
-$("#home_login_form").css('display','block');
+// $(document).ready(function() {
+//   $('.login').on('click', function(event){
+//     event.preventDefault();
+//     console.log("displaay")
+//   $("#home_login_form").css('visibility','visible');
+//   $("#home_login_form").css('display','block');
 
-})
+//   })
+// });
+
+$(document).ready(function() {
+function showLoginForm(){
+ console.log("showLoginForm is called")
+
+ // setTimeout(function(){
+  $("#home-login-form").show()//},44);
+}
+  $("#home-login").click(function(event) {
+    event.preventDefault();
+    console.log("sign in button event")
+    showLoginForm();
+
+  });
 });
+
+
+
+
+
