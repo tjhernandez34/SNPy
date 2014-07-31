0f5da39f18c2a2ab55450013faa6864adb16f95b
Osama Zayyad & Travis Hernandez <pair+ozayyad.tjhernandez34@devbootcamp.com>
2014-07-30 23:29:25 -0500

    refactored a bit

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -9,23 +9,4 @@ class DiseasesController < ApplicationController
     @disease = Disease.find(params[:id])
   end
 
-  # def barchart
-  #   @disease = Disease.find(params[:id])
-  #   @risks = current_user.current_risks_by_disease["#{@disease.name}"]
-  #   data = [[],[]]
-  #   @risks.each_with_index do |risk, index|
-
-  #     if risk.marker.risk_level > 0
-  #       data[0] << {value: risk.marker.risk_level, name: risk.marker.allele, group: "Negative"}
-  #     else
-  #       data[1] << {value: risk.marker.risk_level.abs, name: risk.marker.allele, group: "Positve"}
-  #     end
-  #   end
-  #   render json: data
-  # end
-  
-  # def sortedbarchart
-  #   @risks = current_user.current_risks.delete_if{|risk| risk.marker.risk_level < 0 }
-  # end
-
 end
