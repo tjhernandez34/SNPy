9d84064adab9f1814425e25c57d11bb36365c1ea
Travis Hernandez <tjhernandez34@gmail.com>
2014-07-31 22:26:53 -0500

    remove disease controller

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,12 +1,3 @@
 class DiseasesController < ApplicationController
 
-  def index
-    @diseases = Disease.where('category_id = ?', params[:category_id])
-  end
-
-  def show
-    @risks = current_user.genomes.last.reports.last.risks
-    @disease = Disease.find(params[:id])
-  end
-
 end
