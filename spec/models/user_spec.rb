97377c130e8e35d7e7ce4d8bc3006acfaa766111
Natalie McCroy <nataliemccroy@gmail.com>
2014-07-31 20:48:30 -0500

    write tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,38 +1,34 @@
 require "spec_helper"
 
 describe User do 
-  # before(:each) do
   it "is valid with a first_name, last_name, email, and password_digest" do 
     @user = User.new(first_name: "Jerry", last_name: "Berry", email: "jb@email.com", password_digest: "password")
     expect(@user).to be_valid 
   end
 
-  xit "is valid with a first_name, last_name, email, and password_digest" do
-    @sample_user.should be_valid
-    expect(@sample_user).to be_valid
+  it "is invalid without a first_name" do
+    expect(User.new(first_name: nil)).to_not be_valid
   end
 
-  xit "is invalid without a first_name" do
-    expect(@sample_user).to have(1).errors_on(:first_name)
+  it "is invalid without a last_name" do
+    expect(User.new(last_name: nil)).to_not be_valid
   end
 
-  xit "is invalid without a last_name" do
-    @sample_user.last_name = ""
-    @sample_user.should_not be_valid
+  it "is invalid without an email" do
+    expect(User.new(email: nil)).to_not be_valid
   end
 
-  xit "is invalid without an email" do
-    @sample_user.email = ""
-    @sample_user.should_not be_valid
+  it "is invalid with a duplicate email address" do
+    User.create(first_name: "Max", last_name: "Ruby", email: "jewel@gmail.com", password_digest: "sweet")
+    user = User.new(first_name: "Jewel", last_name: "Green", email: "jewel@gmail.com", password_digest: "cake")
+      expect(user).to_not be_valid
   end
 
-  xit "should not be valid if missing password_digest" do
-    @sample_user.password_digest = ""
-    @sample_user.should_not be_valid
+  it "should not be valid if missing password_digest" do
+    expect(User.new(last_name: nil)).to_not be_valid
   end
 
-  xit { should validate_uniqueness_of :email }
-  xit { should have_many :genomes }
-  xit { should have_many(:reports).through(:genomes) }
+  it { should have_many :genomes }
+  it { should have_many(:reports).through(:genomes) }
 end
 
